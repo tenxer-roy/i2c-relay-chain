9883a6fac30177adfc4326d04f0b8f72616415e8
Roy <roy@tenxerlabs.com>
2023-10-18 18:20:56 +0530

    commenting the the code

--- Relay_Test_F.py
+++ Relay_Test_F.py
@@ -1,48 +1,48 @@
-import smbus
+import smbus                                                                #import necessary libraries
 import time
 
-class RelayController:
-    def __init__(self):
-        self.bus = smbus.SMBus(1)
+class RelayController:                                                      #create class RelayController
+    def __init__(self):       
+        self.bus = smbus.SMBus(1)                                           #Initializing i2cbus
         self.D = []
         self.V = []
 
-    def initialize(self, num_devices):
+    def initialize(self, num_devices):                                       #Initializing the relay module
         self.D = []
         self.V = []
-        for i in range(num_devices):
-            address = input(f"Enter the address for device no. {i + 1}: ")
+        for i in range(num_devices):   
+            address = input(f"Enter the address for device no. {i + 1}: ")   #enter the address of all i2c devices
             self.D.append(address)
             self.V.append(0xFF)
-            self.bus.write_byte(int(self.D[i], 16), self.V[i])
+            self.bus.write_byte(int(self.D[i], 16), self.V[i])               #write into the bus (device address, data to write)
 
     def process_command(self, command):
         if len(command) < 3:
-            print("Wrong instruction! Please review the command format.")
+            print("Wrong instruction! Please review the command format.")     #Address value length should be 2
             return
 
         fst_ltr = command[0]
         rly_num = command[1:-1]
         lst_ltr = command[-1]
 
-        if not (fst_ltr.lower() == "r" and (lst_ltr.lower() in ["n", "f"])):
+        if not (fst_ltr.lower() == "r" and (lst_ltr.lower() in ["n", "f"])):   #enter "r" followed by relay number to turn on or turn off relay.
             print("Wrong instruction! Please review the command format.")
             return
         
         if(rly_num.isnumeric()==False):
-            print("Failed! Relay number out of range.")
+            print("Failed! Relay number out of range.")                        #if entered relay value is not a numeric value
             return
 
         rly_num = int(rly_num)
         if (rly_num == 0):
-            print("Failed! Relay number out of range.")
+            print("Failed! Relay number out of range.")                        #if entered relay value is 0
             return
 
         module_num = (rly_num - 1) // 8
-        pin_num = (rly_num - 1) % 8
+        pin_num = (rly_num - 1) % 8                                            #to check which relay needs to trigger
 
         if module_num >= len(self.D):
-            print("Failed! Relay number out of range.")
+            print("Failed! Relay number out of range.")                        #
             return
 
         print("module_num:", module_num + 1)
