46924ae6546596e526cb64de1c80d8e9c8eb86a3
Roy <roy@tenxerlabs.com>
2023-10-19 11:06:20 +0530

    Commenting the code

--- Relay_Test_F.py
+++ Relay_Test_F.py
@@ -7,31 +7,31 @@ class RelayController:                                                      #cre
         self.D = []
         self.V = []
 
-    def initialize(self, num_devices):                                       #Initializing the relay module
+    def initialize(self, num_devices):                                      #Initializing the relay module
         self.D = []
         self.V = []
         for i in range(num_devices):   
-            address = input(f"Enter the address for device no. {i + 1}: ")   #enter the address of all i2c devices
-            self.D.append(address)
-            self.V.append(0xFF)
-            self.bus.write_byte(int(self.D[i], 16), self.V[i])               #write into the bus (device address, data to write)
+            address = input(f"Enter the address for device no. {i + 1}: ")  #enter the address of all i2c devices
+            self.D.append(address)                                          #append the addresses in D[] array 
+            self.V.append(0xFF)                                             #fill the values with 11111111, initial condition OFF
+            self.bus.write_byte(int(self.D[i], 16), self.V[i])              #write into the bus (device address, data to write)
 
     def process_command(self, command):
         if len(command) < 3:
-            print("Wrong instruction! Please review the command format.")     #Address value length should be 2
+            print("Wrong instruction! Please review the command format.")   #Address value length must be >=3
             return
 
-        fst_ltr = command[0]
-        rly_num = command[1:-1]
-        lst_ltr = command[-1]
+        fst_ltr = command[0]                                                
+        rly_num = command[1:-1]                                                 #relay number
+        lst_ltr = command[-1]                                                   #on off command
 
-        if not (fst_ltr.lower() == "r" and (lst_ltr.lower() in ["n", "f"])):   #enter "r" followed by relay number to turn on or turn off relay.
+        if not (fst_ltr.lower() == "r" and (lst_ltr.lower() in ["n", "f"])):    #enter "r" followed by relay number to turn on or turn off relay.
             print("Wrong instruction! Please review the command format.")
             return
         
         if(rly_num.isnumeric()==False):
-            print("Failed! Relay number out of range.")                        #if entered relay value is not a numeric value
-            return
+            print("Failed! Relay number out of range.")                         #if entered relay value is not a numeric value
+            return                                                              # continue to 
 
         rly_num = int(rly_num)
         if (rly_num == 0):
